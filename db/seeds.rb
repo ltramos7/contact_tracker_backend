b6ef9afc6d3dc140ba519c1a652f6d240c25f359
Linda Ramos <ltramos7@gmail.com>
2021-05-17 18:26:02 -0600

    created seed data for connections

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 
 user_one = User.create(first_name: "Jane", last_name: "Smite", location: "Colorado")
 user_two = User.create(first_name: "Michael", last_name: "Haves", location: "Illinois")
+user_three = User.create(first_name: "Molly", last_name: "Manriquez", location: "California")
+user_four = User.create(first_name: "Jim", last_name: "Zed", location: "Washington")
+
+connection_one = Connection.create(user_one: user_two.id, user_two: user_four.id)
+connection_two = Connection.create(user_one: user_four.id, user_two: user_one.id)
+connection_three = Connection.create(user_one: user_one.id, user_two: user_three.id)
